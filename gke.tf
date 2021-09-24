e68c6611da7254af41e244c1db0c55cbdb327a99
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-09-24 21:02:57 +0530

    Add files via upload

---
+++ gke.tf
@@ -0,0 +1,71 @@
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
+
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "tf-gke"
+  location = "us-west1"
+  project                 = "lyrical-bolt-318719"
+  
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = "us-west1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+  project                 = "lyrical-bolt-318719"
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = "poc"
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "tf-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+module "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id   = "lyrical-bolt-318719"
+  location     = "us-west1"
+  cluster_name = "tf-gke"
+}
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig"
+}
+
+
+
---
+++ vpc.tf
@@ -0,0 +1,19 @@
+provider "google" {
+  credentials = "${file("C:\\Users\\nkolli\\Documents\\Terraform\\GCP\\credentials.json")}"
+}
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "gke-vpc"
+  project                 = "lyrical-bolt-318719"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "gke-subnet"
+  project       = "lyrical-bolt-318719"
+  region        = "us-west1"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
