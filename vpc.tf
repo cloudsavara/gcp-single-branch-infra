329a017c7e800f5de2df64e0331a03912ad41562
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-09-26 13:49:51 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,14 +5,14 @@ provider "google" {
 # VPC
 resource "google_compute_network" "vpc" {
   name                    = "gke-vpc"
-  project                 = var.PROJECT_ID
+  project                 = var.project
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = "gke-subnet"
-  project       = var.PROJECT_ID
+  project       = var.project
   region        = "us-west1"
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
